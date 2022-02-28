736eedd3cc0e26e131f36bf124c6279e727ab6e6
elena1001 <elena971001@gmail.com>
2022-02-28 13:20:35 +0800

    增加漢堡選單jquery

--- js/all.js
+++ js/all.js
@@ -1,8 +1,10 @@
 
-$(function(){
+$(document).ready(function(){
 
     $("button.hamburger_icon").on("click", function(){
       $(this).toggleClass("-on");
+      $('body').toggleClass('menu-show');
     });
-    
-  });
+
+});
+
