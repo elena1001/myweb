e9818ec1af6ff2d6ec43aa12f49953bce2230fe4
elena1001 <elena971001@gmail.com>
2022-02-28 19:48:54 +0800

    刪除商品js

--- js/all.js
+++ js/all.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
 
 });
 
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.addEventListener("click",function(e){
+      if(e.target.classList.contains("trash")){
+        let is_delete = confirm("確認刪除？");
+    
+          if(is_delete){
+    
+            e.target.closest("ul").classList.add("fade_out");
+            setTimeout(function(){
+              e.target.closest("ul").remove();
+            },1000);
+            
+          }
+      }
+  });
+
+
+
+
+});
+
